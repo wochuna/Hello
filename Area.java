a80791d7879f2d71cc5bf2e6ed16d992608468e4
WOCHUNA <wnamusomi@kabarak.ac.ke>
2024-01-19 16:24:45 +0300

    two

---
+++ Area.java
@@ -0,0 +1,12 @@
+public class Area {
+    public static void main(String[] args) {
+    double pi=0;
+    int r=700 ;
+    int l=623 ;
+    int Area= r*l;
+    System.out.println("Area of a cone:" +Area);
+    
+    
+    }     
+    
+}
