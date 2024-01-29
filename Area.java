fc83f2455ef3f99645ee53dad6b9d6febd3b0ca6
WOCHUNA <wnamusomi@kabarak.ac.ke>
2024-01-29 20:48:24 +0300

    another

--- Area.java
+++ Area.java
@@ -1,12 +1,17 @@
+import java.lang.Math;
+import java.util.Scanner;
 public class Area {
-    public static void main(String[] args) {
-    double pi=0;
-    int r=700 ;
-    int l=623 ;
-    int Area= r*l;
-    System.out.println("Area of a cone:" +Area);
-    
-    
+      public static void main(String[]args){
+      //r and l.
+      double area;
+      int r= 623;
+      int l=700;
+      //calulate the area using PI.
+      area=r*l* Math.PI;
+      System.out.println("Area of a cone:" +area);
+      Scanner  scanner =new Scanner(System.in);
+           String name = scanner.nextLine();
+        System.out.println(r +"radius");
+        System.out.println(l +"length");
     }     
-    
 }
---
+++ Pi.java
@@ -0,0 +1,10 @@
+ import java.lang.Math;
+ public class Pi {
+    public static void main(String[]args){
+    //radius and length.
+    double radius=700;
+    double length=63;
+    //calulate the area using PI.
+    double area=radius*length* Math.PI;
+}
+}
